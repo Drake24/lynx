9e129e3187f8fd154c709a3f0b24a9b9cd59433e
paolo.nunal <paolonikko.nunal@wsp.com>
2025-04-21 13:28:19 +1200

    init package

---
+++ Providers/LynxServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drake24\Lynx\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InspirationProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Lynx.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drake24\Lynx;
+
+
+class Lynx
+{
+    public function connect()
+    {
+        return 'hello world';
+    }
+}
